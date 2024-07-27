d4fe93f1c4e3d9ece80e8345bfaaa663c9441c2b
PoojaSharma1801 <pp4776501@gmail.com>
2024-07-27 11:15:15 +0530

    Swap program

---
+++ pg2.js
@@ -0,0 +1,11 @@
+let x = 7;
+let y = 5;
+let max;
+
+if (x > y) {
+  max = x;
+} else {
+  max = y;
+}
+
+console.log(max); // Output will be 7
---
+++ pg3.js
@@ -0,0 +1,4 @@
+let num = 4; // Replace 4 with any integer
+let isEven = (num % 2 === 0);
+
+console.log(isEven); // Output will be true for even numbers, false for odd numbers
